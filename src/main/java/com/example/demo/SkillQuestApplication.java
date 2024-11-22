fb8361faef44429ddaff4b586918bf6a7069a6b2
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-11-22 20:26:49 -0300

    update

---
+++ src/main/java/com/example/demo/SkillQuestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillQuestApplication.class, args);
+	}
+
+}
